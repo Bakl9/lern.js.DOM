85234581634f4ab8ab4a93873129e0fbc13527c1
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-10 20:42:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var div = document.querySelector('#playground');
+var p = documen.querySelectorAll('.text');
+var h1 = document.querySelector('h1');
+var ul = document.querySelector('div#playground > div ul');
+
+console.log(div)
+console.log(p)
+console.log(h1)
+console.log(ul)
