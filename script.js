1be954c0187bcc2ccba0a18dece298cc35154f49
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-12 08:44:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ a.setAttribute('href', 'https://ya.ru');
 a.textContent = 'Yandex'
 
 console.log(a.attributes);
+
+////////////////////////////////////////////////////////////////////////////////////////
+var box1 = document.querySelector('#box1');
+var box2 = document.querySelector('#box2');
+
+box1.classList.add('red');
+var hasClass = box2.classList.contains('orang');
+console.log(hasClass);
+
+if (hassClass) {
+    box2.classList.remove('orang');
+} else {
+    box2.classList.add('orang');
+}
