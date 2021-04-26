cabacf61b8d437e40375fb6281eabd31d4753d71
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-26 17:05:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,10 @@ button.addEventListener('click', buttonClick);
 function buttonClick() {
     console.log('cklicked!');
 };
+var divs = document.querySelectorAll('div');
+
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', function() {
+         console.log(this.getAttribute('id'))
+    }, true);
+};
